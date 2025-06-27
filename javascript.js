a03cd21a91bf01ef650a70ac5a5603df7c159eb9
Tomas Merino <tomasmerinomm@gmail.com>
2025-06-27 00:47:57 +0000

    Adds basic functions for the calculator

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//DOM manipulation
+
+//Calculator functions
+
+function addition(a, b) {
+    return a + b;
+}
+
+function substraction(a, b) {
+    return a - b;
+}
+
+function multiplication (a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function pow(a, b) {
+    return Math.pow(a, b);
+}
+
+function sqrt(a) {
+    return Math.sqrt(a);
+}
